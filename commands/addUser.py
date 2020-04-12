9559f3a0528fff04341425c5d43b3e969cd62935
arnavn101 <arnav.nidumolu@gmail.cogit config --global user.name Your>
2020-04-12 02:45:38 +0000

    Changes made

--- commands/addUser.py
+++ commands/addUser.py
@@ -2,7 +2,7 @@ from commands.base_command  import BaseCommand
 from utils                  import get_emoji
 from random                 import randint
 import os, sys
-import database
+import database, brawlstats
 
 class RegisterUser(BaseCommand):
 
@@ -10,7 +10,7 @@ class RegisterUser(BaseCommand):
         # A quick description for the help message
         description = "Adds a User to Club's Database"
         # A list of parameters that the command will take as input
-        # Parameters will be separated by spaces and fed to the 'params' 
+        # Parameters will be separated by spaces and fed to the 'params'
         # argument in the handle() method
         # If no params are expected, leave this list empty or set it to None
         params = ["player_tag"]
@@ -19,27 +19,30 @@ class RegisterUser(BaseCommand):
     # Override the handle() method
     # It will be called every time the command is received
     async def handle(self, params, message, client):
-        # 'params' is a list that contains the parameters that the command 
+        # 'params' is a list that contains the parameters that the command
         # expects to receive, t is guaranteed to have AT LEAST as many
         # parameters as specified in __init__
         # 'message' is the discord.py Message object for the command to handle
         # 'client' is the bot Client object
-
+        brawl_client = brawlstats.Client("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk5MzM5YTRlLWFmYzItNDJhYy04ZTg1LWE1Y2JlOWE4YjhkOSIsImlhdCI6MTU4NjY1ODc3NSwic3ViIjoiZGV2ZWxvcGVyL2Q0OGFlYWJmLWQ4Y2MtNTMxYi02OTAwLTk5NmI4MjQ2ZWMzMCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTk4LjE5OS42Ny45NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.25aIHp26Or2tPG6xAcd_CYx5rRAePm7uVDWOjyO-pOgRy4ymkNhZqd7ikcZG5ma35J1pEi-UhzQXR4W6uiX20Q")
         try:
             player_tag = str(params[0])
-            
+            print(player_tag)
+            if player_tag[:1] != "#":
+                raise Exception("Incorrect Player Tag. Be sure to add #")
+            specific_user = brawl_client.get_profile(player_tag)
         except Exception:
             await client.send_message(message.channel,
                                       "Please, provide valid player tag")
             return
-        
+
         brawl_database = database.SQL_Server()
         user_name = str(message.author)
         if user_name in brawl_database.return_allUsers():
             await client.send_message(message.channel,
                         "The User has already been linked to an account")
             return
-        
+
         if player_tag in brawl_database.return_allPlayers():
             await client.send_message(message.channel,
                             "The Tag has already been linked to other user")
@@ -50,3 +53,4 @@ class RegisterUser(BaseCommand):
         msg = get_emoji(":ok_hand:") + f" The User {player_tag} has been linked to {user_name} in the database!"
 
         await client.send_message(message.channel, msg)
+
--- events/regulate_event.py
+++ events/regulate_event.py
@@ -28,7 +28,7 @@ class RegulateServer(BaseEvent):
 
         warning_channel = get_channel(client, "warnings")
         brawl_database = database.SQL_Server()
-        
+
         if "ClubTag" not in brawl_database.return_allUsers():
                 msg = "@everyone Club Tag not added to the database"
                 await client.send_message(warning_channel , msg)
@@ -40,13 +40,13 @@ class RegulateServer(BaseEvent):
                 role          = [get(member_object.server.roles, name="member")]
 
                 if member_object.bot or get(member_object.roles, name="owner"):
-                    pass    
+                    pass
 
                 elif brawl_database.return_userWarning(member) >= self.allowed_userWarning and get(member_object.roles, name="member"):
                     msg = " removed from server since they did not insert player information in database"
                     await client.send_message(warning_channel , member_object.mention + msg)
                     await client.remove_roles(member_object, *role)
-            
+
                 elif member not in brawl_database.return_allUsers():
                     brawl_database.insert_user_warning(member)
                     msg = " did not insert player information in database"
@@ -56,16 +56,17 @@ class RegulateServer(BaseEvent):
                     brawl_database.append_user_warning(member)
                     msg = " did not insert player information in database"
                     await client.send_message(warning_channel , member_object.mention + msg)
-            
+
                 elif get(member_object.roles, name="member"):
-                    brawl_client = brawlstats.Client("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImFmOTEyZGRlLTMxMjAtNDc4Zi05OWQ0LWFmMjk4YzhmZmE1NyIsImlhdCI6MTU4NjExNzIzNCwic3ViIjoiZGV2ZWxvcGVyL2Q0OGFlYWJmLWQ4Y2MtNTMxYi02OTAwLTk5NmI4MjQ2ZWMzMCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNjguMTkzLjIxMy4yMTUiXSwidHlwZSI6ImNsaWVudCJ9XX0.m5koS7UWOUgapjEdGP4ddTQnJOYLnquEIH6JDGs0xW8HHuQ1_mrR212zl5EM4ORL6aByhpIHXILfwt5ptmTWxA")
-                    list_users = brawl_client.get_club_members(brawl_database.view_information_user("ClubTag"))
+                    brawl_client = brawlstats.Client("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk5MzM5YTRlLWFmYzItNDJhYy04ZTg1LWE1Y2JlOWE4YjhkOSIsImlhdCI6MTU4NjY1ODc3NSwic3ViIjoiZGV2ZWxvcGVyL2Q0OGFlYWJmLWQ4Y2MtNTMxYi02OTAwLTk5NmI4MjQ2ZWMzMCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTk4LjE5OS42Ny45NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.25aIHp26Or2tPG6xAcd_CYx5rRAePm7uVDWOjyO-pOgRy4ymkNhZqd7ikcZG5ma35J1pEi-UhzQXR4W6uiX20Q")
+                    with open('tags.txt', 'r') as f:
+                        brawl_tag = f.read().splitlines()
+                    list_users = []
+                    list_users.extend(brawl_client.get_club_members(brawl_database.view_information_user("ClubTag")))
+                    for tag in brawl_tag:
+                        list_users.extend(brawl_client.get_club_members(tag))
                     if member not in list_users:
                         msg = " removed from server since they are not part of the club"
                         await client.send_message(warning_channel , member_object.mention + msg)
                         await client.remove_roles(member_object, *role)
-                        
 
-
-        
-    
