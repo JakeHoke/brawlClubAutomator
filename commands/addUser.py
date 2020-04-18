86902f97dfcf60bafdac587ad2c4745178964e93
arnavn101 <arnav.nidumolu@gmail.com>
2020-04-18 14:35:35 -0400

    Changes made

--- commands/addUser.py
+++ commands/addUser.py
@@ -9,11 +9,15 @@ class RegisterUser(BaseCommand):
     def __init__(self):
         # A quick description for the help message
         description = "Adds a User to Club's Database"
+        self.config = configparser.ConfigParser()
+        self.config.read('config.cfg')
+        self.brawl_api = str(self.config.get('ConfigInfo', 'API_KEY'))
         # A list of parameters that the command will take as input
         # Parameters will be separated by spaces and fed to the 'params'
         # argument in the handle() method
         # If no params are expected, leave this list empty or set it to None
         params = ["player_tag"]
+
         super().__init__(description, params)
 
     # Override the handle() method
@@ -24,7 +28,7 @@ class RegisterUser(BaseCommand):
         # parameters as specified in __init__
         # 'message' is the discord.py Message object for the command to handle
         # 'client' is the bot Client object
-        brawl_client = brawlstats.Client("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk5MzM5YTRlLWFmYzItNDJhYy04ZTg1LWE1Y2JlOWE4YjhkOSIsImlhdCI6MTU4NjY1ODc3NSwic3ViIjoiZGV2ZWxvcGVyL2Q0OGFlYWJmLWQ4Y2MtNTMxYi02OTAwLTk5NmI4MjQ2ZWMzMCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTk4LjE5OS42Ny45NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.25aIHp26Or2tPG6xAcd_CYx5rRAePm7uVDWOjyO-pOgRy4ymkNhZqd7ikcZG5ma35J1pEi-UhzQXR4W6uiX20Q")
+        brawl_client = brawlstats.Client(self.brawl_api)
         try:
             player_tag = str(params[0])
             print(player_tag)
--- events/regulate_event.py
+++ events/regulate_event.py
@@ -15,6 +15,7 @@ class RegulateServer(BaseEvent):
         self.interval_minutes = float(self.config.get('ConfigInfo', 'TIME_INTERVAL'))  # Set the interval for this event
         var = float(self.config.get('ConfigInfo', 'ALLOWED_TIME'))
         self.allowed_userWarning = math.ceil(var/self.interval_minutes)
+        self.brawl_api = str(self.config.get('ConfigInfo', 'API_KEY'))
         print(f"User Warning {self.allowed_userWarning}")
         super().__init__(self.interval_minutes)
 
@@ -58,7 +59,7 @@ class RegulateServer(BaseEvent):
                     await client.send_message(warning_channel , member_object.mention + msg)
 
                 elif get(member_object.roles, name="member"):
-                    brawl_client = brawlstats.Client("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk5MzM5YTRlLWFmYzItNDJhYy04ZTg1LWE1Y2JlOWE4YjhkOSIsImlhdCI6MTU4NjY1ODc3NSwic3ViIjoiZGV2ZWxvcGVyL2Q0OGFlYWJmLWQ4Y2MtNTMxYi02OTAwLTk5NmI4MjQ2ZWMzMCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTk4LjE5OS42Ny45NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.25aIHp26Or2tPG6xAcd_CYx5rRAePm7uVDWOjyO-pOgRy4ymkNhZqd7ikcZG5ma35J1pEi-UhzQXR4W6uiX20Q")
+                    brawl_client = brawlstats.Client(self.brawl_api)
                     with open('tags.txt', 'r') as f:
                         brawl_tag = f.read().splitlines()
                     list_users = []
